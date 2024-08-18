52e3fed654426256f55a0fc758fc0639e2758b7b
Kayce Basques <kaycebasques@gmail.com>
2024-08-18 10:37:37 -0700

    Initial commit

---
+++ build.py
@@ -0,0 +1,45 @@
+import json
+import os
+import pathlib
+import shutil
+import sys
+
+import requests
+
+cwd = os.getcwd()
+zip_name = 'out'
+zip_path = f'{cwd}/{zip_name}.zip'
+src_dir = 'www'
+src_path = f'{cwd}/{src_dir}'
+
+if os.path.exists(zip_path):
+    os.remove(zip_path)
+
+with open(f'{src_path}/screenshots/usernames.json', 'r') as f:
+    usernames = json.load(f)
+
+for username in usernames:
+    screenshot_path = f'{src_path}/screenshots/{username}.jpg'
+    if os.path.exists(screenshot_path):
+        continue
+    url = f'https://nekoweb.org/screenshots/{username}/index_large.jpg'
+    response = requests.get(url, stream=True)
+    with open(screenshot_path, 'wb') as f:
+        shutil.copyfileobj(response.raw, f)
+    # TODO: Optimize the images - https://stackoverflow.com/a/13211834
+
+shutil.make_archive(zip_name, 'zip', src_path)
+
+# TODO: Detect NEKOWEB_API_KEY in environment rather than argv length
+if len(sys.argv) == 2:
+    base = 'https://nekoweb.org/api'
+    api_key = sys.argv[1]
+    headers = {'Authorization': api_key}
+    response = requests.get(f'{base}/files/big/create', headers=headers)
+    data = response.json()
+    big_file_id = data['id']
+    # https://requests.readthedocs.io/en/latest/user/quickstart/#post-a-multipart-encoded-file
+    files = {'file': open(zip_path, 'rb')}
+    data = {'id': big_file_id}
+    response = requests.post(f'{base}/files/big/append', headers=headers, files=files, data=data)
+    response = requests.post(f'{base}/files/import/{big_file_id}', headers=headers)
---
+++ dev.sh
@@ -0,0 +1,3 @@
+source venv/bin/activate
+python3 build.py
+python3 -m http.server -d www
